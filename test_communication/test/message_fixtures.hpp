871b05ddb540c0f4c6162c9c883e2474ee2e7e0f
Dirk Thomas <dthomas@osrfoundation.org>
2015-07-29 16:18:27 -0700

    fix more windows warnings

--- test_communication/test/message_fixtures.hpp
+++ test_communication/test/message_fixtures.hpp
@@ -16,6 +16,7 @@
 #define __test_communication__message_fixtures__hpp__
 
 #include <cassert>
+#include <limits>
 #include <vector>
 
 #include <test_communication/msg/builtins.hpp>
@@ -63,17 +64,17 @@ get_messages_primitives()
     auto msg = std::make_shared<test_communication::msg::Primitives>();
     msg->bool_value = true;
     msg->byte_value = 255;
-    msg->char_value = 0xff;
+    msg->char_value = '\255';
     msg->float32_value = 1.11f;
     msg->float64_value = 1.11;
-    msg->int8_value = 127;
-    msg->uint8_value = 255;
-    msg->int16_value = 32767;
-    msg->uint16_value = 65535;
-    msg->int32_value = 2147483647;
-    msg->uint32_value = 4294967295;
-    msg->int64_value = 9223372036854775807;
-    msg->uint64_value = 18446744073709551615UL;
+    msg->int8_value = (std::numeric_limits<int8_t>::max)();
+    msg->uint8_value = (std::numeric_limits<uint8_t>::max)();
+    msg->int16_value = (std::numeric_limits<int16_t>::max)();
+    msg->uint16_value = (std::numeric_limits<uint16_t>::max)();
+    msg->int32_value = (std::numeric_limits<int32_t>::max)();
+    msg->uint32_value = (std::numeric_limits<uint32_t>::max)();
+    msg->int64_value = (std::numeric_limits<int64_t>::max)();
+    msg->uint64_value = (std::numeric_limits<uint64_t>::max)();
     msg->string_value = "max value";
     messages.push_back(msg);
   }
@@ -84,13 +85,13 @@ get_messages_primitives()
     msg->char_value = 0x0;
     msg->float32_value = -2.22f;
     msg->float64_value = -2.22;
-    msg->int8_value = -128;
+    msg->int8_value = (std::numeric_limits<int8_t>::min)();
     msg->uint8_value = 0;
-    msg->int16_value = -32768;
+    msg->int16_value = (std::numeric_limits<int16_t>::min)();
     msg->uint16_value = 0;
-    msg->int32_value = -2147483648;
+    msg->int32_value = (std::numeric_limits<int32_t>::min)();
     msg->uint32_value = 0;
-    msg->int64_value = -9223372036854775808UL;
+    msg->int64_value = (std::numeric_limits<int64_t>::min)();
     msg->uint64_value = 0;
     msg->string_value = "min value";
     messages.push_back(msg);
@@ -109,20 +110,22 @@ get_messages_static_array_primitives()
     msg->char_values = {{'\0', '\255', '\0'}};
     msg->float32_values = {{0.0f, 1.11f, -2.22f}};
     msg->float64_values = {{0, 1.11, -2.22}};
-    msg->int8_values = {{0, 127, -128}};
-    msg->uint8_values = {{0, 255, 0}};
-    msg->int16_values = {{0, 32767, -32768}};
-    msg->uint16_values = {{0, 65535, 0}};
+    msg->int8_values = {{
+      0, (std::numeric_limits<int8_t>::max)(), (std::numeric_limits<int8_t>::min)()}};
+    msg->uint8_values = {{0, (std::numeric_limits<uint8_t>::max)(), 0}};
+    msg->int16_values = {{
+      0, (std::numeric_limits<int16_t>::max)(), (std::numeric_limits<int16_t>::min)()}};
+    msg->uint16_values = {{0, (std::numeric_limits<uint16_t>::max)(), 0}};
     msg->int32_values = {{
       static_cast<int32_t>(0),
-      static_cast<int32_t>(2147483647),
-      static_cast<int32_t>(-2147483648)
+      (std::numeric_limits<int32_t>::max)(),
+      (std::numeric_limits<int32_t>::min)()
     }};
-    msg->uint32_values = {{0, 4294967295, 0}};
+    msg->uint32_values = {{0, (std::numeric_limits<uint32_t>::max)(), 0}};
     msg->int64_values[0] = 0;
-    msg->int64_values[1] = 9223372036854775807;
-    msg->int64_values[2] = -9223372036854775808UL;
-    msg->uint64_values = {{0, 18446744073709551615UL, 0}};
+    msg->int64_values[1] = (std::numeric_limits<int64_t>::max)();
+    msg->int64_values[2] = (std::numeric_limits<int64_t>::min)();
+    msg->uint64_values = {{0, (std::numeric_limits<uint64_t>::max)(), 0}};
     msg->string_values = {{"", "max value", "min value"}};
     messages.push_back(msg);
   }
@@ -158,14 +161,14 @@ get_messages_dynamic_array_primitives()
     msg->char_values = {'\255'};
     msg->float32_values = {1.11f};
     msg->float64_values = {1.11};
-    msg->int8_values = {127};
-    msg->uint8_values = {255};
-    msg->int16_values = {32767};
-    msg->uint16_values = {65535};
-    msg->int32_values = {2147483647};
-    msg->uint32_values = {4294967295};
-    msg->int64_values = {9223372036854775807};
-    msg->uint64_values = {18446744073709551615UL};
+    msg->int8_values = {(std::numeric_limits<int8_t>::max)()};
+    msg->uint8_values = {(std::numeric_limits<uint8_t>::max)()};
+    msg->int16_values = {(std::numeric_limits<int16_t>::max)()};
+    msg->uint16_values = {(std::numeric_limits<uint16_t>::max)()};
+    msg->int32_values = {(std::numeric_limits<int32_t>::max)()};
+    msg->uint32_values = {(std::numeric_limits<uint32_t>::max)()};
+    msg->int64_values = {(std::numeric_limits<int64_t>::max)()};
+    msg->uint64_values = {(std::numeric_limits<uint64_t>::max)()};
     msg->string_values = {{"max value"}};
     messages.push_back(msg);
   }
@@ -176,22 +179,24 @@ get_messages_dynamic_array_primitives()
     msg->char_values = {{'\0', '\255'}};
     msg->float32_values = {{0.0f, 1.11f, -2.22f}};
     msg->float64_values = {{0, 1.11, -2.22}};
-    msg->int8_values = {{0, 127, -128}};
-    msg->uint8_values = {{0, 255}};
-    msg->int16_values = {{0, 32767, -32768}};
-    msg->uint16_values = {{0, 65535}};
+    msg->int8_values = {{
+      0, (std::numeric_limits<int8_t>::max)(), (std::numeric_limits<int8_t>::min)()}};
+    msg->uint8_values = {{0, (std::numeric_limits<uint8_t>::max)()}};
+    msg->int16_values = {{
+      0, (std::numeric_limits<int16_t>::max)(), (std::numeric_limits<int16_t>::min)()}};
+    msg->uint16_values = {{0, (std::numeric_limits<uint16_t>::max)()}};
     // The narrowing static cast is required to avoid build errors on Windows.
     msg->int32_values = {{
       static_cast<int32_t>(0),
-      static_cast<int32_t>(2147483647),
-      static_cast<int32_t>(-2147483648)
+      (std::numeric_limits<int32_t>::max)(),
+      (std::numeric_limits<int32_t>::min)()
     }};
-    msg->uint32_values = {{0, 4294967295}};
+    msg->uint32_values = {{0, (std::numeric_limits<uint32_t>::max)()}};
     msg->int64_values.resize(3);
     msg->int64_values[0] = 0;
-    msg->int64_values[1] = 9223372036854775807;
-    msg->int64_values[2] = -9223372036854775808UL;
-    msg->uint64_values = {{0, 18446744073709551615UL}};
+    msg->int64_values[1] = (std::numeric_limits<int64_t>::max)();
+    msg->int64_values[2] = (std::numeric_limits<int64_t>::min)();
+    msg->uint64_values = {{0, (std::numeric_limits<uint64_t>::max)()}};
     msg->string_values = {{"", "max value", "optional min value"}};
     messages.push_back(msg);
   }
