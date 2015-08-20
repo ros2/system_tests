f8fb645439ce08f7f9a810deea9943439c2b41ab
Esteve Fernandez <esteve@apache.org>
2015-08-20 09:48:52 -0700

    Added test for QoS durability

---
+++ test_communication/test/test_qos_publisher.cpp
@@ -0,0 +1,69 @@
+// Copyright 2015 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "message_fixtures.hpp"
+
+template<typename T>
+void publish(
+  rclcpp::Node::SharedPtr node,
+  std::vector<typename T::SharedPtr> messages,
+  size_t number_of_cycles = 5)
+{
+  auto start = std::chrono::steady_clock::now();
+
+  rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
+  custom_qos_profile.depth = messages.size();
+  custom_qos_profile.durability = RMW_QOS_POLICY_DURABILITY_PERSISTENT;
+
+  auto publisher = node->create_publisher<T>(
+    std::string("test_qos_message_primitives"), custom_qos_profile);
+
+  rclcpp::WallRate time_between_cycles(1);
+  rclcpp::WallRate time_between_messages(10);
+  size_t cycle_index = 0;
+  // publish all messages up to number_of_cycles times, longer sleep between each cycle
+  while (rclcpp::ok() && cycle_index < number_of_cycles) {
+    size_t message_index = 0;
+    // publish all messages one by one, shorter sleep between each message
+    while (rclcpp::ok() && message_index < messages.size()) {
+      std::cout << "publishing message #" << (message_index + 1) << std::endl;
+      publisher->publish(messages[message_index]);
+      ++message_index;
+      time_between_messages.sleep();
+    }
+    ++cycle_index;
+    time_between_cycles.sleep();
+  }
+
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<float> diff = (end - start);
+  std::cout << "published for " << diff.count() << " seconds" << std::endl;
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto node = rclcpp::Node::make_shared(std::string("test_qos_publisher_primitives"));
+
+  publish<test_communication::msg::Primitives>(node, get_messages_primitives(), 1);
+
+  return 0;
+}
---
+++ test_communication/test/test_qos_subscriber.cpp
@@ -0,0 +1,107 @@
+// Copyright 2015 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "message_fixtures.hpp"
+
+
+template<typename T>
+rclcpp::subscription::SubscriptionBase::SharedPtr subscribe(
+  rclcpp::Node::SharedPtr node,
+  std::vector<typename T::SharedPtr> & expected_messages,
+  std::vector<bool> & received_messages)
+{
+  received_messages.assign(expected_messages.size(), false);
+
+  auto callback =
+    [&expected_messages, &received_messages](const typename T::SharedPtr received_message) -> void
+    {
+      // find received message in vector of expected messages
+      auto received = received_messages.begin();
+      bool known_message = false;
+      size_t index = 0;
+      for (auto expected_message : expected_messages) {
+        if (*received_message == *expected_message) {
+          *received = true;
+          std::cout << "received message #" << (index + 1) << " of " <<
+            expected_messages.size() << std::endl;
+          known_message = true;
+          break;
+        }
+        ++received;
+        ++index;
+      }
+      if (!known_message) {
+        fprintf(stderr, "received message does not match any expected message\n");
+        rclcpp::shutdown();
+        throw std::runtime_error("received message does not match any expected message");
+      }
+
+      // shutdown node when all expected messages have been received
+      for (auto received : received_messages) {
+        if (!received) {
+          return;
+        }
+      }
+      rclcpp::shutdown();
+    };
+
+  rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
+  custom_qos_profile.depth = expected_messages.size();
+  custom_qos_profile.durability = RMW_QOS_POLICY_DURABILITY_PERSISTENT;
+
+  auto subscriber = node->create_subscription<T>(
+    std::string("test_qos_message_primitives"), custom_qos_profile, callback);
+  return subscriber;
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto start = std::chrono::steady_clock::now();
+
+  auto node = rclcpp::Node::make_shared(std::string("test_qos_subscriber_primitives"));
+
+  rclcpp::subscription::SubscriptionBase::SharedPtr subscriber;
+  std::vector<bool> received_messages;  // collect flags about received messages
+
+  auto messages = get_messages_primitives();
+
+  std::cout << "Wait for 20 seconds before subscribing" << std::endl;
+
+  // Wait for a while before subscribing
+  std::chrono::seconds wait_time(20);
+
+  std::this_thread::sleep_for(wait_time);
+
+  std::cout << "Create subscriber" << std::endl;
+
+  subscriber = subscribe<test_communication::msg::Primitives>(
+    node, messages, received_messages);
+
+  rclcpp::spin(node);
+
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<float> diff = (end - start);
+  std::cout << "subscribed for " << diff.count() << " seconds" << std::endl;
+
+  return 0;
+}
