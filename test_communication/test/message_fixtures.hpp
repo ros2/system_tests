29e7610c491a1a2ac314d07770480d4db2aa1987
Mikael Arguedas <mikael@osrfoundation.org>
2016-09-14 13:45:52 -0700

    Ascii fixture (#161)
    * use ASCII value for char
    
    * homogenize BoundedArrayPrimitives fixtures

--- test_communication/test/message_fixtures.hpp
+++ test_communication/test/message_fixtures.hpp
@@ -66,7 +66,7 @@ get_messages_primitives()
     auto msg = std::make_shared<test_communication::msg::Primitives>();
     msg->bool_value = true;
     msg->byte_value = 255;
-    msg->char_value = '\255';
+    msg->char_value = '\x7f';
     msg->float32_value = 1.125f;
     msg->float64_value = 1.125;
     msg->int8_value = (std::numeric_limits<int8_t>::max)();
@@ -131,7 +131,7 @@ get_messages_static_array_primitives()
     auto msg = std::make_shared<test_communication::msg::StaticArrayPrimitives>();
     msg->bool_values = {{false, true, false}};
     msg->byte_values = {{0, 0xff, 0}};
-    msg->char_values = {{'\0', '\255', '\0'}};
+    msg->char_values = {{'\0', '\x7f', '\0'}};
     msg->float32_values = {{0.0f, 1.125f, -2.125f}};
     msg->float64_values = {{0, 1.125, -2.125}};
     // *INDENT-OFF* (prevent uncrustify from making unnecessary indents here)
@@ -185,7 +185,7 @@ get_messages_dynamic_array_primitives()
     auto msg = std::make_shared<test_communication::msg::DynamicArrayPrimitives>();
     msg->bool_values = {true};
     msg->byte_values = {0xff};
-    msg->char_values = {'\255'};
+    msg->char_values = {'\x7f'};
     msg->float32_values = {1.125f};
     msg->float64_values = {1.125};
     msg->int8_values = {(std::numeric_limits<int8_t>::max)()};
@@ -204,7 +204,7 @@ get_messages_dynamic_array_primitives()
     auto msg = std::make_shared<test_communication::msg::DynamicArrayPrimitives>();
     msg->bool_values = {{false, true}};
     msg->byte_values = {{0, 0xff}};
-    msg->char_values = {{'\0', '\255'}};
+    msg->char_values = {{'\0', '\x7f'}};
     msg->float32_values = {{0.0f, 1.125f, -2.125f}};
     msg->float64_values = {{0, 1.125, -2.125}};
     // *INDENT-OFF* (prevent uncrustify from making unnecessary indents here)
@@ -285,7 +285,7 @@ get_messages_bounded_array_primitives()
     auto msg = std::make_shared<test_communication::msg::BoundedArrayPrimitives>();
     msg->bool_values = {{false, true, false}};
     msg->byte_values = {{0, 1, 0xff}};
-    msg->char_values = {{'\0', '\1', '\255'}};
+    msg->char_values = {{'\0', '\1', '\x7f'}};
     msg->float32_values = {{0.0f, 1.125f, -2.125f}};
     msg->float64_values = {{0, 1.125, -2.125}};
     // *INDENT-OFF* (prevent uncrustify from making unnecessary indents here)
