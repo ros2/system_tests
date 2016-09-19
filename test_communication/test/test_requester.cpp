2bc10972c43c6b58740e9a52af3b0daa84ee9e08
William Woodall <william@osrfoundation.org>
2016-09-19 16:00:01 -0700

    remove no longer needed sleep

--- test_communication/test/test_requester.cpp
+++ test_communication/test/test_requester.cpp
@@ -109,9 +109,6 @@ int main(int argc, char ** argv)
   std::string service = argv[1];
   auto node = rclcpp::Node::make_shared(std::string("test_requester_") + service);
 
-  // NOTE(esteve): sleep for two seconds to let the service start up
-  std::this_thread::sleep_for(std::chrono::seconds(2));
-
   int rc;
   if (service == "Empty") {
     rc = request<test_communication::srv::Empty>(
