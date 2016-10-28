a0477eca2a4a09192277f5e1d2532f7b36f7b787
William Woodall <william@osrfoundation.org>
2016-10-27 17:34:55 -0700

    update wait_for_subscriber to also wait for it to be gone

--- test_rclcpp/include/test_rclcpp/utils.hpp
+++ test_rclcpp/include/test_rclcpp/utils.hpp
@@ -28,28 +28,40 @@ namespace test_rclcpp
 {
 
 // Sleep for timeout ms or until a subscriber has registered for the topic
+// if to_be_available is true, then it will wait for the number of
+// subscribers to be > 0, if false it will wait for the number of
+// subscribers to be 0
 void wait_for_subscriber(
   std::shared_ptr<rclcpp::Node> node,
   const std::string & topic_name,
+  bool to_be_available = true,
   std::chrono::milliseconds timeout = std::chrono::milliseconds(1),
   std::chrono::microseconds sleep_period = std::chrono::seconds(1))
 {
   using std::chrono::duration_cast;
   using std::chrono::microseconds;
   using std::chrono::steady_clock;
+  auto start = steady_clock::now();
   microseconds time_slept(0);
-  while (node->count_subscribers(topic_name) == 0 &&
-    time_slept < duration_cast<microseconds>(timeout))
-  {
-    auto start = steady_clock::now();
+  auto predicate = [&node, &topic_name, &to_be_available]() -> bool {
+    if (to_be_available) {
+      // the subscriber is available if the count is gt 0
+      return node->count_subscribers(topic_name) > 0;
+    } else {
+      // the subscriver is no longer available when the count is 0
+      return node->count_subscribers(topic_name) == 0;
+    }
+  };
+  while (!predicate() && time_slept < duration_cast<microseconds>(timeout)) {
     rclcpp::event::Event::SharedPtr graph_event = node->get_graph_event();
     node->wait_for_graph_change(graph_event, sleep_period);
-    time_slept += duration_cast<std::chrono::microseconds>(steady_clock::now() - start);
+    time_slept = duration_cast<std::chrono::microseconds>(steady_clock::now() - start);
   }
   int64_t time_slept_count =
     std::chrono::duration_cast<std::chrono::microseconds>(time_slept).count();
-  printf("Waited %" PRId64 " microseconds for the subscriber to connect to topic '%s'\n",
+  printf("Waited %" PRId64 " microseconds for the subscriber to %s topic '%s'\n",
     time_slept_count,
+    to_be_available ? "connect to" : "disconnect from",
     topic_name.c_str());
 }
 
