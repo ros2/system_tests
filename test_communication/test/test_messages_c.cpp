8b4f08a26528a2900965e2b11586b43a803441e2
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-12-13 08:47:42 -0800

    more fixture update

--- test_communication/test/test_messages_c.cpp
+++ test_communication/test/test_messages_c.cpp
@@ -263,7 +263,7 @@ void get_message(test_communication__msg__Primitives * msg, size_t msg_num)
       break;
     case 1:
       msg->bool_value = true;
-      msg->byte_value = 255;
+      msg->byte_value = 127;
       msg->char_value = '\x7f';
       msg->float32_value = 1.125f;
       msg->float64_value = 1.125;
@@ -279,7 +279,7 @@ void get_message(test_communication__msg__Primitives * msg, size_t msg_num)
       break;
     case 2:
       msg->bool_value = false;
-      msg->byte_value = 0;
+      msg->byte_value = -128;
       msg->char_value = 0x0;
       msg->float32_value = -2.125f;
       msg->float64_value = -2.125;
@@ -444,10 +444,10 @@ void get_message(test_communication__msg__StaticArrayPrimitives * msg, size_t ms
     msg->bool_values[1] = true;
     msg->bool_values[2] = false;
     msg->byte_values[0] = 0;
-    msg->byte_values[1] = 0xff;
-    msg->byte_values[2] = 0;
+    msg->byte_values[1] = 127;
+    msg->byte_values[2] = -128;
     msg->char_values[0] = '\0';
-    msg->char_values[1] = '\x7f';
+    msg->char_values[1] = '\xff';
     msg->char_values[2] = '\0';
     msg->float32_values[0] = 0.0f;
     msg->float32_values[1] = 1.125f;
@@ -612,8 +612,8 @@ void get_message(test_communication__msg__DynamicArrayPrimitives * msg, size_t m
       rosidl_generator_c__String__Array__init(&msg->string_values, 1);
 
       msg->bool_values.data[0] = true;
-      msg->byte_values.data[0] = 127;
-      msg->char_values.data[0] = 255;
+      msg->byte_values.data[0] = 0x7f;
+      msg->char_values.data[0] = '\xff';
       msg->float32_values.data[0] = 1.125f;
       msg->float64_values.data[0] = 1.125;
       msg->int8_values.data[0] = (std::numeric_limits<int8_t>::max)();
@@ -645,8 +645,8 @@ void get_message(test_communication__msg__DynamicArrayPrimitives * msg, size_t m
 
       msg->bool_values.data[0] = false;
       msg->bool_values.data[1] = true;
-      msg->byte_values.data[0] = 0x00;
-      msg->byte_values.data[1] = 0x7f;
+      msg->byte_values.data[0] = -128;
+      msg->byte_values.data[1] = 127;
       msg->char_values.data[0] = '\0';
       msg->char_values.data[1] = '\xff';
       msg->float32_values.data[0] = 0.0f;
@@ -921,8 +921,8 @@ void get_message(test_communication__msg__BoundedArrayPrimitives * msg, size_t m
       msg->bool_values.data[1] = true;
       msg->bool_values.data[2] = false;
       msg->byte_values.data[0] = 0x00;
-      msg->byte_values.data[1] = 0x01;
-      msg->byte_values.data[2] = 0xff;
+      msg->byte_values.data[1] = 127;
+      msg->byte_values.data[2] = -128;
       msg->char_values.data[0] = '\0';
       msg->char_values.data[1] = '\1';
       msg->char_values.data[2] = '\255';
