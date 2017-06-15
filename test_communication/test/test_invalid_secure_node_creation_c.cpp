7797f3a8d9abeb349ce468395538338902e4c4e7
Mikael Arguedas <mikael@osrfoundation.org>
2017-06-14 19:50:24 -0700

    Adding security tests (#204)
    * WIP: add security tests
    
    * keys, certs and crap used for testing
    
    * switching to a multi process test because of https://github.com/eProsima/Fast-RTPS/issues/106
    
    * test failing / throwing cases
    
    * test only for fastrtps for now
    
    * lint
    
    * unnused var name
    
    * WIP
    
    * test all message type for regression checking. Also disable should throw examples that will be implemented in a single process in C
    
    * update certs/key files
    
    * move tests with invalid node creation to single process
    
    * add not connecting tests with timer, remove unused args, simplify template logic
    
    * remove now useless topic_name parameters
    
    * leverage VALID_SECURE_ROOT
    
    * more cleanup
    
    * update copyright year
    
    * remove debug prints
    
    * remove unused variables
    
    * add generated from notice to all test python templates
    
    * removing variables is great, code that compiles is better
    
    * check for test target existence
    
    * rename test suite to match what is being tested
    
    * rename security environment variables
    
    * trailing whitespace

---
+++ test_communication/test/test_invalid_secure_node_creation_c.cpp
@@ -0,0 +1,139 @@
+// Copyright 2016-2017 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gtest/gtest.h>
+
+#include <functional>
+#include <limits>
+#include <memory>
+#include <string>
+
+#include "rcl/rcl.h"
+#include "rcl/error_handling.h"
+#include "rcutils/get_env.h"
+
+#ifndef SCOPE_EXIT_HPP_
+#define SCOPE_EXIT_HPP_
+
+template<typename Callable>
+struct ScopeExit
+{
+  explicit ScopeExit(Callable callable)
+  : callable_(callable) {}
+  ~ScopeExit() {callable_(); }
+
+private:
+  Callable callable_;
+};
+
+template<typename Callable>
+ScopeExit<Callable>
+make_scope_exit(Callable callable)
+{
+  return ScopeExit<Callable>(callable);
+}
+
+void custom_putenv(const char * name, const char * value)
+{
+#ifdef WIN32
+  _putenv_s(name, value);
+#else
+  setenv(name, value, 1);
+#endif
+}
+
+#define SCOPE_EXIT(code) make_scope_exit([&]() {code; })
+
+#endif  // SCOPE_EXIT_HPP_
+
+
+#ifdef RMW_IMPLEMENTATION
+# define CLASSNAME_(NAME, SUFFIX) NAME ## __ ## SUFFIX
+# define CLASSNAME(NAME, SUFFIX) CLASSNAME_(NAME, SUFFIX)
+#else
+# define CLASSNAME(NAME, SUFFIX) NAME
+#endif
+
+class CLASSNAME (TestInvalidSecureNode, RMW_IMPLEMENTATION) : public ::testing::Test
+{
+public:
+  rcl_node_t * node_ptr;
+  void SetUp()
+  {
+  }
+
+  void TearDown()
+  {
+    rcl_ret_t ret = rcl_node_fini(this->node_ptr);
+    delete this->node_ptr;
+    ret = rcl_shutdown();
+    EXPECT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+  }
+
+  void test_node_creation(
+    const char * ROS_SECURITY_ROOT_DIRECTORY, const char * ROS_SECURITY_ENABLE,
+    const char * ROS_SECURITY_STRATEGY,
+    const char * node_name, bool should_fail_participant_creation)
+  {
+    if (ROS_SECURITY_ROOT_DIRECTORY != NULL) {
+      custom_putenv("ROS_SECURITY_ROOT_DIRECTORY", ROS_SECURITY_ROOT_DIRECTORY);
+    }
+    custom_putenv("ROS_SECURITY_ENABLE", ROS_SECURITY_ENABLE);
+    custom_putenv("ROS_SECURITY_STRATEGY", ROS_SECURITY_STRATEGY);
+    rcl_ret_t ret;
+    ret = rcl_init(0, nullptr, rcl_get_default_allocator());
+    ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    this->node_ptr = new rcl_node_t;
+    *this->node_ptr = rcl_get_zero_initialized_node();
+    // const char * name = "node_name";
+    rcl_node_options_t node_options = rcl_node_get_default_options();
+    ret = rcl_node_init(this->node_ptr, node_name, "", &node_options);
+    if (should_fail_participant_creation) {
+      ASSERT_EQ(RCL_RET_ERROR, ret) << rcl_get_error_string_safe();
+    } else {
+      ASSERT_EQ(RCL_RET_OK, ret) << rcl_get_error_string_safe();
+    }
+  }
+};
+
+TEST_F(CLASSNAME(TestInvalidSecureNode, RMW_IMPLEMENTATION), test_1) {
+  test_node_creation(
+    "garbage",
+    "true", "Enforce",
+    "publisher", true);
+}
+
+TEST_F(CLASSNAME(TestInvalidSecureNode, RMW_IMPLEMENTATION), test_2) {
+  test_node_creation(
+    NULL,
+    "true", "Enforce",
+    "publisher2", true);
+}
+
+TEST_F(CLASSNAME(TestInvalidSecureNode, RMW_IMPLEMENTATION), test_3) {
+  test_node_creation(
+    NULL,
+    "true", "Enforce",
+    "publisher_invalid_cert", true);
+}
+
+TEST_F(CLASSNAME(TestInvalidSecureNode, RMW_IMPLEMENTATION), test_4) {
+  test_node_creation(
+    NULL,
+    "true", "Enforce",
+    "publisher_missing_key", true);
+}
---
+++ test_communication/test/test_secure_publisher.cpp
@@ -0,0 +1,121 @@
+// Copyright 2015-2017 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "message_fixtures.hpp"
+
+template<typename T>
+int8_t attempt_publish(
+  rclcpp::Node::SharedPtr node,
+  const std::string & topic_name,
+  std::vector<typename T::SharedPtr> messages,
+  size_t number_of_cycles = 100)
+{
+  auto start = std::chrono::steady_clock::now();
+
+  rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
+  custom_qos_profile.depth = messages.size();
+
+  auto publisher = node->create_publisher<T>(
+    topic_name, custom_qos_profile);
+
+  rclcpp::WallRate cycle_rate(10);
+  rclcpp::WallRate message_rate(100);
+  size_t cycle_index = 0;
+  // publish all messages up to number_of_cycles times, longer sleep between each cycle
+  while (rclcpp::ok() && cycle_index < number_of_cycles) {
+    size_t message_index = 0;
+    // publish all messages one by one, shorter sleep between each message
+    while (rclcpp::ok() && message_index < messages.size()) {
+      std::cout << "publishing message #" << (message_index + 1) << std::endl;
+      publisher->publish(messages[message_index]);
+      ++message_index;
+      message_rate.sleep();
+    }
+    ++cycle_index;
+    cycle_rate.sleep();
+  }
+
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<float> diff = (end - start);
+  std::cout << "published for " << diff.count() << " seconds" << std::endl;
+
+  return 0;
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  if (argc != 2) {
+    fprintf(
+      stderr,
+      "Wrong number of arguments,\n"
+      "pass a message type\n");
+    return 1;
+  }
+  std::string message = argv[1];
+  std::string node_name = "publisher";
+  std::string topic_name = "chatter";
+  std::shared_ptr<rclcpp::node::Node> node = nullptr;
+  try {
+    node = rclcpp::Node::make_shared(node_name);
+  } catch (std::runtime_error &) {
+    fprintf(stderr, "should not have thrown!");
+  }
+  fprintf(stderr, "node created, attempt to publish");
+  int8_t ret;
+
+  if (message == "Empty") {
+    ret = attempt_publish<test_communication::msg::Empty>(
+      node, topic_name, get_messages_empty());
+  } else if (message == "Primitives") {
+    ret = attempt_publish<test_communication::msg::Primitives>(
+      node, topic_name, get_messages_primitives());
+  } else if (message == "StaticArrayPrimitives") {
+    ret = attempt_publish<test_communication::msg::StaticArrayPrimitives>(
+      node, topic_name, get_messages_static_array_primitives());
+  } else if (message == "DynamicArrayPrimitives") {
+    ret = attempt_publish<test_communication::msg::DynamicArrayPrimitives>(
+      node, topic_name, get_messages_dynamic_array_primitives());
+  } else if (message == "BoundedArrayPrimitives") {
+    ret = attempt_publish<test_communication::msg::BoundedArrayPrimitives>(
+      node, topic_name, get_messages_bounded_array_primitives());
+  } else if (message == "Nested") {
+    ret = attempt_publish<test_communication::msg::Nested>(
+      node, topic_name, get_messages_nested());
+  } else if (message == "DynamicArrayNested") {
+    ret = attempt_publish<test_communication::msg::DynamicArrayNested>(
+      node, topic_name, get_messages_dynamic_array_nested());
+  } else if (message == "BoundedArrayNested") {
+    ret = attempt_publish<test_communication::msg::BoundedArrayNested>(
+      node, topic_name, get_messages_bounded_array_nested());
+  } else if (message == "StaticArrayNested") {
+    ret = attempt_publish<test_communication::msg::StaticArrayNested>(
+      node, topic_name, get_messages_static_array_nested());
+  } else if (message == "Builtins") {
+    ret = attempt_publish<test_communication::msg::Builtins>(
+      node, topic_name, get_messages_builtins());
+  } else {
+    fprintf(stderr, "Unknown message argument '%s'\n", message.c_str());
+    return 1;
+  }
+  return ret;
+}
