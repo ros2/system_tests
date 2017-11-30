c627f4bae05480966aa9f8ea7a8fbad3a7dad1a2
Shane Loretz <sloretz@osrfoundation.org>
2017-11-30 14:57:43 -0800

    Use plain method

--- test_communication/test/requester_py.py
+++ test_communication/test/requester_py.py
@@ -16,41 +16,44 @@ import argparse
 import importlib
 import sys
 
-import rclpy
-from rclpy.node import Node
-from test_msgs.service_fixtures import get_test_srv
 
+def requester(service_name):
+    import rclpy
+    from test_msgs.service_fixtures import get_test_srv
+    rclpy.init(args=[])
 
-class Requester(Node):
-
-    def __init__(self, service_name):
-        super().__init__('requester')
+    try:
         # Import the service
         service_pkg = 'test_msgs'
         module = importlib.import_module(service_pkg + '.srv')
-        self.srv_mod = getattr(module, service_name)
+        srv_mod = getattr(module, service_name)
 
-        self.srv_fixtures = get_test_srv(service_name)
-        self.service_name = 'test_service_' + service_name
+        srv_fixtures = get_test_srv(service_name)
+        service_name = 'test_service_' + service_name
 
-    def run(self):
-        # wait for the service to be available
-        client = node.create_client(self.srv_mod, self.service_name)
-        tries = 15
-        while rclpy.ok() and not client.wait_for_service(timeout_sec=1.0) and tries > 0:
-            print('service not available, waiting again...')
-            tries -= 1
-        assert tries > 0
+        node = rclpy.create_node('requester')
+        try:
+            # wait for the service to be available
+            client = node.create_client(srv_mod, service_name)
+            tries = 15
+            while rclpy.ok() and not client.wait_for_service(timeout_sec=1.0) and tries > 0:
+                print('service not available, waiting again...')
+                tries -= 1
+            assert tries > 0
 
-        print('requester: beginning request')
-        # Make one call to that service
-        for req, resp in self.srv_fixtures:
-            client.call(req)
-            client.wait_for_future()
-            assert repr(client.response) == repr(resp), \
-                'unexpected response %r\n\nwas expecting %r' % (client.response, resp)
-            print('received reply #%d of %d' % (
-                self.srv_fixtures.index([req, resp]) + 1, len(self.srv_fixtures)))
+            print('requester: beginning request')
+            # Make one call to that service
+            for req, resp in srv_fixtures:
+                client.call(req)
+                client.wait_for_future()
+                assert repr(client.response) == repr(resp), \
+                    'unexpected response %r\n\nwas expecting %r' % (client.response, resp)
+                print('received reply #%d of %d' % (
+                    srv_fixtures.index([req, resp]) + 1, len(srv_fixtures)))
+        finally:
+            node.destroy_node()
+    finally:
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
@@ -59,16 +62,9 @@ if __name__ == '__main__':
                         help='name of the ROS message')
     args = parser.parse_args()
     try:
-        rclpy.init(args=[])
-        node = Requester(args.service_name)
-        try:
-            node.run()
-        finally:
-            node.destroy_node()
+        requester(service_name=args.service_name)
     except KeyboardInterrupt:
         print('requester stopped cleanly')
     except BaseException:
         print('exception in requester:', file=sys.stderr)
         raise
-    finally:
-        rclpy.shutdown()
