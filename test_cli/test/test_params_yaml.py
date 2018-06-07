4441214e9751f6ffbab0e55be105ddcbfbb34a19
Shane Loretz <sloretz@osrfoundation.org>
2018-06-07 14:28:47 -0700

    Use threading instead of asyncio

--- test_cli/test/test_params_yaml.py
+++ test_cli/test/test_params_yaml.py
@@ -12,14 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
-
 import pytest
 from rcl_interfaces.msg import ParameterType
 from rcl_interfaces.srv import GetParameters
 import rclpy
 
-from .utils import launch_process_and_coroutine
+from .utils import BackgroundExecutor
+from .utils import HelperCommand
 from .utils import NamedTemporaryFile
 from .utils import require_environment_variable
 
@@ -44,272 +43,209 @@ def node_fixture(request):
         rclpy.shutdown()
 
 
-def get_params_test(node, check_func, param_names, *, attempts=10, time_between_attempts=1.0):
-    """Make a test that gets params and calls check_func(results)."""
-    async def coroutine_test():
-        nonlocal node
-        nonlocal check_func
-        nonlocal param_names
-        nonlocal attempts
-        nonlocal time_between_attempts
-        # wait for service to be ready
-        client = node.create_client(GetParameters, '/initial_params_node/get_parameters')
-        while not client.service_is_ready() and attempts > 0:
-            attempts -= 1
-            await asyncio.sleep(time_between_attempts)
-
-        assert attempts > 0
-
-        # Call the service and get the results
+def get_params(node, node_name, param_names, wfs_timeout=5.0):
+    with BackgroundExecutor(node):
+        client = node.create_client(GetParameters, '/{name}/get_parameters'.format(name=node_name))
+        assert client.wait_for_service(timeout_sec=wfs_timeout)
         request = GetParameters.Request()
         request.names = param_names
-        future = client.call_async(request)
-        for _ in range(attempts):
-            if future.done():
-                break
-            rclpy.spin_once(node)
-            await asyncio.sleep(time_between_attempts)
-        if future.exception():
-            raise future.exception()
-
-        assert attempts > 0
-
-        # see if the result looks ok
-        check_func(future.result())
-
-    return coroutine_test
+        resp = client.call(request)
+        node.destroy_client(client)
+        return resp
 
 
 def test_bool_params(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
+    param_file_content = """
+bool_params:
+    ros__parameters:
+        b1: False
+        b2: True
+"""
+    with NamedTemporaryFile(param_file_content) as yaml_file:
+        command = (node_fixture['executable'], '__params:=' + yaml_file, '__node:=bool_params')
+
+        with HelperCommand(command):
+            resp = get_params(node_fixture['node'], 'bool_params', ['b1', 'b2'])
+
         assert 2 == len(resp.values)
         assert ParameterType.PARAMETER_BOOL == resp.values[0].type
         assert ParameterType.PARAMETER_BOOL == resp.values[1].type
         assert not resp.values[0].bool_value
         assert resp.values[1].bool_value
 
-    with NamedTemporaryFile() as yaml_file:
-        yaml_file.write("""
-initial_params_node:
-    ros__parameters:
-        b1: False
-        b2: True
-""")
-        yaml_file.flush()
-        # close so it can be opened again on windows
-        yaml_file.close()
-
-        command = (node_fixture['executable'], '__params:=' + yaml_file.name)
-        actual_test = get_params_test(node_fixture['node'], check_params, ('b1', 'b2'))
-        assert 0 == launch_process_and_coroutine(command, actual_test)
-
 
 def test_integer_params(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
+    param_file_content = """
+int_params:
+    ros__parameters:
+        i1: 42
+        i2: -27
+"""
+    with NamedTemporaryFile(param_file_content) as yaml_file:
+        command = (node_fixture['executable'], '__params:=' + yaml_file, '__node:=int_params')
+
+        with HelperCommand(command):
+            resp = get_params(node_fixture['node'], 'int_params', ['i1', 'i2'])
+
         assert 2 == len(resp.values)
         assert ParameterType.PARAMETER_INTEGER == resp.values[0].type
         assert ParameterType.PARAMETER_INTEGER == resp.values[1].type
         assert 42 == resp.values[0].integer_value
         assert -27 == resp.values[1].integer_value
 
-    with NamedTemporaryFile() as yaml_file:
-        yaml_file.write("""
-initial_params_node:
-    ros__parameters:
-        i1: 42
-        i2: -27
-""")
-        yaml_file.flush()
-        yaml_file.close()
-
-        command = (node_fixture['executable'], '__params:=' + yaml_file.name)
-        actual_test = get_params_test(node_fixture['node'], check_params, ('i1', 'i2'))
-        assert 0 == launch_process_and_coroutine(command, actual_test)
-
 
 def test_double_params(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
+    param_file_content = """
+double_params:
+    ros__parameters:
+        d1: 3.14
+        d2: -2.718
+"""
+    with NamedTemporaryFile(param_file_content) as yaml_file:
+        command = (node_fixture['executable'], '__params:=' + yaml_file, '__node:=double_params')
+
+        with HelperCommand(command):
+            resp = get_params(node_fixture['node'], 'double_params', ['d1', 'd2'])
+
         assert 2 == len(resp.values)
         assert ParameterType.PARAMETER_DOUBLE == resp.values[0].type
         assert ParameterType.PARAMETER_DOUBLE == resp.values[1].type
         assert pytest.approx(3.14) == resp.values[0].double_value
         assert pytest.approx(-2.718) == resp.values[1].double_value
 
-    with NamedTemporaryFile() as yaml_file:
-        yaml_file.write("""
-initial_params_node:
-    ros__parameters:
-        d1: 3.14
-        d2: -2.718
-""")
-        yaml_file.flush()
-        yaml_file.close()
-
-        command = (node_fixture['executable'], '__params:=' + yaml_file.name)
-        actual_test = get_params_test(node_fixture['node'], check_params, ('d1', 'd2'))
-        assert 0 == launch_process_and_coroutine(command, actual_test)
-
 
 def test_string_params(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
+    param_file_content = """
+str_params:
+    ros__parameters:
+        s1: hello
+        s2: world
+"""
+    with NamedTemporaryFile(param_file_content) as yaml_file:
+        command = (node_fixture['executable'], '__params:=' + yaml_file, '__node:=str_params')
+
+        with HelperCommand(command):
+            resp = get_params(node_fixture['node'], 'str_params', ['s1', 's2'])
+
         assert 2 == len(resp.values)
         assert ParameterType.PARAMETER_STRING == resp.values[0].type
         assert ParameterType.PARAMETER_STRING == resp.values[1].type
         assert resp.values[0].string_value == 'hello'
         assert resp.values[1].string_value == 'world'
 
-    with NamedTemporaryFile() as yaml_file:
-        yaml_file.write("""
-initial_params_node:
-    ros__parameters:
-        s1: hello
-        s2: world
-""")
-        yaml_file.flush()
-        yaml_file.close()
-
-        command = (node_fixture['executable'], '__params:=' + yaml_file.name)
-        actual_test = get_params_test(node_fixture['node'], check_params, ('s1', 's2'))
-        assert 0 == launch_process_and_coroutine(command, actual_test)
-
 
 # TODO(sloretz) PARAMETER_BYTE_ARRAY when rcl_yaml_param_parser supports it
 
 
 def test_bool_array_params(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
+    param_file_content = """
+ba_params:
+    ros__parameters:
+        ba1: [true, false]
+        ba2: [false, true]
+"""
+    with NamedTemporaryFile(param_file_content) as yaml_file:
+        command = (node_fixture['executable'], '__params:=' + yaml_file, '__node:=ba_params')
+
+        with HelperCommand(command):
+            resp = get_params(node_fixture['node'], 'ba_params', ['ba1', 'ba2'])
+
         assert 2 == len(resp.values)
         assert ParameterType.PARAMETER_BOOL_ARRAY == resp.values[0].type
         assert ParameterType.PARAMETER_BOOL_ARRAY == resp.values[1].type
         assert resp.values[0].bool_array_value == [True, False]
         assert resp.values[1].bool_array_value == [False, True]
 
-    with NamedTemporaryFile() as yaml_file:
-        yaml_file.write("""
-initial_params_node:
-    ros__parameters:
-        ba1: [true, false]
-        ba2: [false, true]
-""")
-        yaml_file.flush()
-        yaml_file.close()
-
-        command = (node_fixture['executable'], '__params:=' + yaml_file.name)
-        actual_test = get_params_test(node_fixture['node'], check_params, ('ba1', 'ba2'))
-        assert 0 == launch_process_and_coroutine(command, actual_test)
-
 
 def test_integer_array_params(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
+    param_file_content = """
+ia_params:
+    ros__parameters:
+        ia1: [42, -27]
+        ia2: [1234, 5678]
+"""
+    with NamedTemporaryFile(param_file_content) as yaml_file:
+        command = (node_fixture['executable'], '__params:=' + yaml_file, '__node:=ia_params')
+
+        with HelperCommand(command):
+            resp = get_params(node_fixture['node'], 'ia_params', ['ia1', 'ia2'])
+
         assert 2 == len(resp.values)
         assert ParameterType.PARAMETER_INTEGER_ARRAY == resp.values[0].type
         assert ParameterType.PARAMETER_INTEGER_ARRAY == resp.values[1].type
         assert resp.values[0].integer_array_value == [42, -27]
         assert resp.values[1].integer_array_value == [1234, 5678]
 
-    with NamedTemporaryFile() as yaml_file:
-        yaml_file.write("""
-initial_params_node:
-    ros__parameters:
-        ia1: [42, -27]
-        ia2: [1234, 5678]
-""")
-        yaml_file.flush()
-        yaml_file.close()
-
-        command = (node_fixture['executable'], '__params:=' + yaml_file.name)
-        actual_test = get_params_test(node_fixture['node'], check_params, ('ia1', 'ia2'))
-        assert 0 == launch_process_and_coroutine(command, actual_test)
-
 
 def test_double_array_params(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
+    param_file_content = """
+da_params:
+    ros__parameters:
+        da1: [3.14, -2.718]
+        da2: [1234.5, -9999.0]
+"""
+    with NamedTemporaryFile(param_file_content) as yaml_file:
+        command = (node_fixture['executable'], '__params:=' + yaml_file, '__node:=da_params')
+
+        with HelperCommand(command):
+            resp = get_params(node_fixture['node'], 'da_params', ['da1', 'da2'])
+
         assert 2 == len(resp.values)
         assert ParameterType.PARAMETER_DOUBLE_ARRAY == resp.values[0].type
         assert ParameterType.PARAMETER_DOUBLE_ARRAY == resp.values[1].type
         assert resp.values[0].double_array_value == pytest.approx([3.14, -2.718])
         assert resp.values[1].double_array_value == pytest.approx([1234.5, -9999.0])
 
-    with NamedTemporaryFile() as yaml_file:
-        yaml_file.write("""
-initial_params_node:
-    ros__parameters:
-        da1: [3.14, -2.718]
-        da2: [1234.5, -9999.0]
-""")
-        yaml_file.flush()
-        yaml_file.close()
-
-        command = (node_fixture['executable'], '__params:=' + yaml_file.name)
-        actual_test = get_params_test(node_fixture['node'], check_params, ('da1', 'da2'))
-        assert 0 == launch_process_and_coroutine(command, actual_test)
-
 
 def test_string_array_params(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
+    param_file_content = """
+sa_params:
+    ros__parameters:
+        sa1: ['Four', 'score']
+        sa2: ['and', 'seven']
+"""
+    with NamedTemporaryFile(param_file_content) as yaml_file:
+        command = (node_fixture['executable'], '__params:=' + yaml_file, '__node:=sa_params')
+
+        with HelperCommand(command):
+            resp = get_params(node_fixture['node'], 'sa_params', ['sa1', 'sa2'])
+
         assert 2 == len(resp.values)
         assert ParameterType.PARAMETER_STRING_ARRAY == resp.values[0].type
         assert ParameterType.PARAMETER_STRING_ARRAY == resp.values[1].type
         assert resp.values[0].string_array_value == ['Four', 'score']
         assert resp.values[1].string_array_value == ['and', 'seven']
 
-    with NamedTemporaryFile() as yaml_file:
-        yaml_file.write("""
-initial_params_node:
-    ros__parameters:
-        sa1: ['Four', 'score']
-        sa2: ['and', 'seven']
-""")
-        yaml_file.flush()
-        yaml_file.close()
-
-        command = (node_fixture['executable'], '__params:=' + yaml_file.name)
-        actual_test = get_params_test(node_fixture['node'], check_params, ('sa1', 'sa2'))
-        assert 0 == launch_process_and_coroutine(command, actual_test)
-
 
 def test_multiple_parameter_files(node_fixture):
-    def check_params(resp):
-        nonlocal node_fixture
-        assert 3 == len(resp.values)
-        assert ParameterType.PARAMETER_INTEGER == resp.values[0].type
-        assert ParameterType.PARAMETER_INTEGER == resp.values[1].type
-        assert ParameterType.PARAMETER_INTEGER == resp.values[2].type
-        assert 42 == resp.values[0].integer_value
-        assert 12345 == resp.values[1].integer_value
-        assert -27 == resp.values[2].integer_value
-
-    with NamedTemporaryFile() as first_yaml_file:
-        first_yaml_file.write("""
-initial_params_node:
+    first_yaml_content = """
+multi_params:
     ros__parameters:
         i1: 42
         i2: -27
-""")
-        first_yaml_file.flush()
-        first_yaml_file.close()
-        with NamedTemporaryFile() as second_yaml_file:
-            second_yaml_file.write("""
-initial_params_node:
+"""
+    second_yaml_content = """
+multi_params:
     ros__parameters:
         i2: 12345
         i3: -27
-""")
-            second_yaml_file.flush()
-            second_yaml_file.close()
+"""
 
+    with NamedTemporaryFile(first_yaml_content) as first_yaml_file:
+        with NamedTemporaryFile(second_yaml_content) as second_yaml_file:
             command = (
                 node_fixture['executable'],
-                '__params:=' + first_yaml_file.name,
-                '__params:=' + second_yaml_file.name
+                '__params:=' + first_yaml_file,
+                '__params:=' + second_yaml_file,
+                '__node:=multi_params'
             )
-            actual_test = get_params_test(node_fixture['node'], check_params, ('i1', 'i2', 'i3'))
-            assert 0 == launch_process_and_coroutine(command, actual_test)
+            with HelperCommand(command):
+                resp = get_params(node_fixture['node'], 'multi_params', ['i1', 'i2', 'i3'])
+
+            assert 3 == len(resp.values)
+            assert ParameterType.PARAMETER_INTEGER == resp.values[0].type
+            assert ParameterType.PARAMETER_INTEGER == resp.values[1].type
+            assert ParameterType.PARAMETER_INTEGER == resp.values[2].type
+            assert 42 == resp.values[0].integer_value
+            assert 12345 == resp.values[1].integer_value
+            assert -27 == resp.values[2].integer_value
--- test_cli/test/utils.py
+++ test_cli/test/utils.py
@@ -12,14 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import copy
 import os
 import random
+import subprocess
 import sys
 import tempfile
+import threading
 
-from launch.legacy import LaunchDescriptor
-from launch.legacy.exit_handler import primary_exit_handler
-from launch.legacy.launcher import DefaultLauncher
+import rclpy
 
 
 def require_environment_variable(name):
@@ -30,49 +31,64 @@ def require_environment_variable(name):
     return env
 
 
-def launch_process_and_coroutine(command, coroutine):
-    """Execute a command and coroutine in parallel."""
-    # Execute python files using same python used to start this test
-    env = dict(os.environ)
-    if command[0][-3:] == '.py':
-        command.insert(0, sys.executable)
-        env['PYTHONUNBUFFERED'] = '1'
+class HelperCommand:
+    """Execute a command in the background."""
 
-    ld = LaunchDescriptor()
-    ld.add_process(
-        cmd=command,
-        name='helper_for_' + coroutine.__name__,
-        env=env
-    )
-    ld.add_coroutine(
-        coroutine(),
-        name=coroutine.__name__,
-        exit_handler=primary_exit_handler
-    )
-    launcher = DefaultLauncher()
-    launcher.add_launch_descriptor(ld)
-    return_code = launcher.launch()
-    return return_code
+    def __init__(self, command):
+        self._env = dict(os.environ)
+        self._command = copy.deepcopy(command)
+
+        # Execute python files using same python used to start this test
+        if command[0][-3:] == '.py':
+            self._command.insert(0, sys.executable)
+            self._env['PYTHONUNBUFFERED'] = '1'
+
+    def __enter__(self):
+        self._proc = subprocess.Popen(self._command, env=self._env)
+        return self
+
+    def __exit__(self, t, v, tb):
+        self._proc.kill()
 
 
 class NamedTemporaryFile:
-    """
-    Create a named temporary file.
+    """Create a named temporary file with content."""
 
-    This allows the file to be closed to allow opening by other processes on windows.
-    """
-
-    def __init__(self, mode='w'):
+    def __init__(self, content):
         self._tempdir = tempfile.TemporaryDirectory(prefix='test_cli_')
-        self._mode = mode
+        self._content = content
 
     def __enter__(self):
         directory = self._tempdir.__enter__()
         name = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(10))
         self._filename = os.path.join(directory, name)
-        self._file = open(self._filename, mode=self._mode)
-        return self._file
+        self._file = open(self._filename, mode='w')
+        self._file.write(self._content)
+        self._file.flush()
+        # close so it can be opened again on windows
+        self._file.close()
+        return self._file.name
 
     def __exit__(self, t, v, tb):
-        self._file.close()
         self._tempdir.__exit__(t, v, tb)
+
+
+class BackgroundExecutor:
+    """Spin an executor in the background."""
+
+    def __init__(self, node, time_between_spins=0.25):
+        self._node = node
+        self._time_between_spins = time_between_spins
+
+    def __enter__(self):
+        self._stop = threading.Event()
+        self._thr = threading.Thread(target=self._run, daemon=True)
+        self._thr.start()
+
+    def _run(self):
+        while not self._stop.is_set():
+            rclpy.spin_once(self._node, timeout_sec=self._time_between_spins)
+
+    def __exit__(self, t, v, tb):
+        self._stop.set()
+        self._thr.join()
