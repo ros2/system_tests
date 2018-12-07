9147079364b96d645f8d63df6f95cf3535ad7dad
William Woodall <william+github@osrfoundation.org>
2018-12-06 18:57:37 -0800

    add Fibonacci test for actions (#316)
    * add Fibonacci test for actions
    
    Signed-off-by: William Woodall <william@osrfoundation.org>
    
    * fixup test creation
    
    * remove debug code
    
    * action tests depend on action client and server
    
    * static cast to get rid of warning

---
+++ test_communication/test/test_action_client.cpp
@@ -0,0 +1,193 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "rclcpp/exceptions.hpp"
+#include "rclcpp/scope_exit.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+#include "test_msgs/action/fibonacci.hpp"
+
+using namespace std::chrono_literals;
+
+template<typename ActionT>
+struct ActionClientTest
+{
+  typename ActionT::Goal goal;
+  std::function<bool(typename ActionT::Result::SharedPtr)> result_is_valid;
+  std::function<bool(typename ActionT::Feedback::ConstSharedPtr)> feedback_is_valid;
+};
+
+template<typename ActionT>
+int
+send_goals(
+  rclcpp::Node::SharedPtr node,
+  const std::string & action_type_name,
+  const std::vector<ActionClientTest<ActionT>> & goal_tests)
+{
+  auto action_client =
+    rclcpp_action::create_client<ActionT>(node, "test/action/" + action_type_name);
+  auto logger = node->get_logger();
+
+  if (!action_client->wait_for_action_server(20s)) {
+    RCLCPP_ERROR(logger, "requester service not available after waiting");
+    return 1;
+  }
+
+  size_t test_index = 0;
+  bool invalid_feedback = false;
+  auto start = std::chrono::steady_clock::now();
+  RCLCPP_SCOPE_EXIT({
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<float> diff = (end - start);
+    RCLCPP_INFO(logger, "sent goals for %f seconds\n", diff.count());
+  });
+
+  while (rclcpp::ok() && test_index < goal_tests.size()) {
+    RCLCPP_INFO(logger, "sending goal #%zu", test_index + 1);
+
+    // on feedback, check the feedback is valid
+    auto feedback_callback =
+      [&](auto, const auto & feedback) {
+        RCLCPP_INFO(logger, "received feedback");
+        if (!goal_tests[test_index].feedback_is_valid(feedback)) {
+          RCLCPP_ERROR(logger, "invalid feedback");
+          invalid_feedback = true;
+        }
+      };
+
+    // send the request
+    auto goal_handle_future =
+      action_client->async_send_goal(goal_tests[test_index].goal, feedback_callback);
+
+    using rclcpp::executor::FutureReturnCode;
+    // wait for the sent goal to be accepted
+    auto status = rclcpp::spin_until_future_complete(node, goal_handle_future, 1000s);
+    if (status != FutureReturnCode::SUCCESS) {
+      RCLCPP_ERROR(logger, "send goal call failed");
+      return 1;
+    }
+
+    // wait for the result (feedback may be received in the meantime)
+    auto result_future = goal_handle_future.get()->async_result();
+    status = rclcpp::spin_until_future_complete(node, result_future, 1000s);
+    if (status != FutureReturnCode::SUCCESS) {
+      RCLCPP_ERROR(logger, "failed to receive a goal result in time");
+      return 1;
+    }
+
+    if (!goal_tests[test_index].result_is_valid(result_future.get().response)) {
+      RCLCPP_ERROR(logger, "invalid goal result");
+      return 1;
+    }
+    RCLCPP_INFO(logger, "received goal #%zu of %zu", test_index + 1, goal_tests.size());
+    test_index++;
+  }
+
+  if (test_index != goal_tests.size()) {
+    return 1;
+  }
+
+  if (invalid_feedback) {
+    return 1;
+  }
+
+  return 0;
+}
+
+std::vector<ActionClientTest<test_msgs::action::Fibonacci>>
+generate_fibonacci_goal_tests()
+{
+  std::vector<ActionClientTest<test_msgs::action::Fibonacci>> result;
+
+  constexpr size_t order = 10;
+  static_assert(order > 0, "order needs to be non-zero");
+
+  std::vector<int32_t> valid_fibo_seq;
+  valid_fibo_seq.push_back(0);
+  valid_fibo_seq.push_back(1);
+  for (size_t i = 1; i < order; ++i) {
+    valid_fibo_seq.push_back(valid_fibo_seq[i] + valid_fibo_seq[i - 1]);
+  }
+
+  {
+    ActionClientTest<test_msgs::action::Fibonacci> test;
+    size_t order = 10;
+    test.goal.order = static_cast<int32_t>(order);
+    test.result_is_valid =
+      [order, valid_fibo_seq](auto result) -> bool {
+        if (result->sequence.size() != (order + 1)) {
+          fprintf(stderr, "result sequence not equal to goal order\n");
+          return false;
+        }
+        for (size_t i = 0; i < order; ++i) {
+          if (valid_fibo_seq[i] != result->sequence[i]) {
+            fprintf(
+              stderr,
+              "result sequence not correct, expected %d but got %d for order %zu\n",
+              valid_fibo_seq[i], result->sequence[i], i);
+            return false;
+          }
+        }
+        return true;
+      };
+    test.feedback_is_valid =
+      [order, valid_fibo_seq](auto feedback) -> bool {
+        if (feedback->sequence.size() > (order + 1)) {
+          fprintf(stderr, "feedback sequence greater than the goal order\n");
+          return false;
+        }
+        for (size_t i = 0; i < feedback->sequence.size(); ++i) {
+          if (valid_fibo_seq[i] != feedback->sequence[i]) {
+            fprintf(
+              stderr,
+              "feedback sequence not correct, expected %d but got %d for order %zu\n",
+              valid_fibo_seq[i], feedback->sequence[i], i);
+            return false;
+          }
+        }
+        return true;
+      };
+    result.push_back(test);
+  }
+
+  return result;
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  if (argc != 3) {
+    fprintf(stderr, "Wrong number of arguments, pass an action type and a namespace\n");
+    return 1;
+  }
+
+  std::string action = argv[1];
+  std::string namespace_ = argv[2];
+  auto node = rclcpp::Node::make_shared("test_action_client_" + action, namespace_);
+
+  int rc;
+  if (action == "Fibonacci") {
+    rc = send_goals<test_msgs::action::Fibonacci>(node, action, generate_fibonacci_goal_tests());
+  } else {
+    fprintf(stderr, "Unknown action type '%s'\n", action.c_str());
+    return 1;
+  }
+  return rc;
+}
---
+++ test_communication/test/test_action_server.cpp
@@ -0,0 +1,198 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+#include "test_msgs/action/fibonacci.hpp"
+
+template<typename ActionT>
+struct ExpectedGoalRequest
+{
+  std::function<bool(std::shared_ptr<const typename ActionT::Goal>)> goal_is_expected;
+  std::function<bool(std::shared_ptr<const typename ActionT::Goal>)> goal_is_valid;
+  std::function<void(std::shared_ptr<rclcpp_action::ServerGoalHandle<ActionT>>)> execute_goal;
+};
+
+bool should_fail = false;
+
+template<typename ActionT>
+typename rclcpp_action::Server<ActionT>::SharedPtr
+receive_goals(
+  rclcpp::Node::SharedPtr node,
+  const std::string & action_type_name,
+  const std::vector<ExpectedGoalRequest<ActionT>> & expected_goal_requests
+)
+{
+  auto logger = node->get_logger();
+
+  auto goal_callback =
+    [expected_goal_requests, logger](auto /* goal_id */, auto goal) {
+      // make sure this goal was expected
+      for (const auto & expected_goal_request : expected_goal_requests) {
+        if (expected_goal_request.goal_is_expected(goal)) {
+          if (!expected_goal_request.goal_is_valid(goal)) {
+            RCLCPP_ERROR(logger, "goal request is invalid");
+            should_fail = true;
+            return rclcpp_action::GoalResponse::REJECT;
+          }
+          return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+        }
+      }
+      RCLCPP_ERROR(logger, "unexpected goal request");
+      should_fail = true;
+      return rclcpp_action::GoalResponse::REJECT;
+    };
+
+  auto cancel_callback =
+    [logger](auto /* goal_handle */) {
+      RCLCPP_ERROR(logger, "received unexpected request to cancel goal");
+      return rclcpp_action::CancelResponse::ACCEPT;
+    };
+
+  auto execute_callback =
+    [expected_goal_requests, logger](auto goal_handle) {
+      auto goal = goal_handle->get_goal();
+      bool known_goal = false;
+      size_t index = 1;
+      for (const auto & expected_goal_request : expected_goal_requests) {
+        if (expected_goal_request.goal_is_expected(goal)) {
+          RCLCPP_INFO(logger, "executing goal #%zu of %zu", index, expected_goal_requests.size());
+          known_goal = true;
+          expected_goal_request.execute_goal(goal_handle);
+          break;
+        }
+        index++;
+      }
+      if (!known_goal) {
+        RCLCPP_ERROR(logger, "unexpected goal which was accepted (should not happen)");
+        should_fail = true;
+      }
+    };
+
+  return rclcpp_action::create_server<ActionT>(
+    node,
+    std::string("test/action/") + action_type_name,
+    goal_callback,
+    cancel_callback,
+    execute_callback);
+}
+
+std::vector<ExpectedGoalRequest<test_msgs::action::Fibonacci>>
+generate_expected_fibonacci_goals(rclcpp::Logger logger)
+{
+  std::vector<ExpectedGoalRequest<test_msgs::action::Fibonacci>> result;
+
+  {
+    ExpectedGoalRequest<test_msgs::action::Fibonacci> expected_goal;
+
+    expected_goal.goal_is_expected =
+      [](auto goal) {
+        if (goal && goal->order == 10) {
+          return true;
+        }
+        return false;
+      };
+
+    expected_goal.goal_is_valid =
+      [](auto) {
+        return true;
+      };
+
+    expected_goal.execute_goal =
+      [logger](auto goal_handle) {
+        const auto goal = goal_handle->get_goal();
+        rclcpp::Rate loop_rate(10);
+
+        auto feedback = std::make_shared<test_msgs::action::Fibonacci::Feedback>();
+        feedback->sequence.push_back(0);
+        feedback->sequence.push_back(1);
+
+        auto result = std::make_shared<test_msgs::action::Fibonacci::Result>();
+
+        if (goal->order <= 0) {
+          RCLCPP_ERROR(logger, "expected a goal > 0, got %d", goal->order);
+          return;
+        }
+        size_t order_size = goal->order;
+
+        for (size_t i = 1; i < order_size; ++i) {
+          if (!rclcpp::ok()) {
+            return;
+          }
+          // Check if the goal was canceled.
+          if (goal_handle->is_canceling()) {
+            result->sequence = feedback->sequence;
+            goal_handle->set_canceled(result);
+            RCLCPP_INFO(logger, "goal was canceled");
+            return;
+          }
+          // Update the sequence.
+          feedback->sequence.push_back(feedback->sequence[i] + feedback->sequence[i - 1]);
+          // Publish the current state as feedback.
+          goal_handle->publish_feedback(feedback);
+          RCLCPP_INFO(logger, "publishing feedback for goal");
+
+          loop_rate.sleep();
+        }
+
+        result->sequence = feedback->sequence;
+        goal_handle->set_succeeded(result);
+        RCLCPP_INFO(logger, "goal succeeded");
+      };
+
+    result.push_back(expected_goal);
+  }
+
+  return result;
+}
+
+int main(int argc, char ** argv)
+{
+  if (argc != 3) {
+    fprintf(stderr, "Wrong number of arguments, pass one action type and a namespace\n");
+    return 1;
+  }
+  rclcpp::init(argc, argv);
+
+  auto start = std::chrono::steady_clock::now();
+
+  std::string action = argv[1];
+  std::string namespace_ = argv[2];
+  auto node = rclcpp::Node::make_shared(std::string("test_action_server_") + action, namespace_);
+
+  rclcpp_action::ServerBase::SharedPtr server;
+
+  if (action == "Fibonacci") {
+    server = receive_goals<test_msgs::action::Fibonacci>(
+      node, action, generate_expected_fibonacci_goals(node->get_logger()));
+  } else {
+    fprintf(stderr, "Unknown action type '%s'\n", action.c_str());
+    return 1;
+  }
+  rclcpp::spin(node);
+
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<float> diff = (end - start);
+  printf("replied for %f seconds\n", diff.count());
+
+  return should_fail ? 1 : 0;
+}
