274659325936c48694cf17750bf1a9a17101d5da
Jacob Hassold <jhassold@dcscorp.com>
2019-05-01 07:37:50 -0400

    Remove unwanted includes
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- test_rclcpp/test/node_check_names.cpp
+++ test_rclcpp/test/node_check_names.cpp
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <algorithm>
 #include <string>
-#include <utility>
-#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 
