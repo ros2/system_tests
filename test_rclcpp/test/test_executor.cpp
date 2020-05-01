bd4e975d23fa5c7da816de415b8b36e9d44cd5d9
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-05-01 16:17:55 -0300

    Make test_executor.spin_some_max_duration more reliable
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- test_rclcpp/test/test_executor.cpp
+++ test_rclcpp/test/test_executor.cpp
@@ -61,7 +61,7 @@ TEST(CLASSNAME(test_executor, RMW_IMPLEMENTATION), spin_some_max_duration) {
   rclcpp::executors::SingleThreadedExecutor executor;
   auto node = rclcpp::Node::make_shared("spin_some_max_duration");
   auto lambda = []() {
-      std::this_thread::sleep_for(1ms);
+      std::this_thread::sleep_for(100ms);
     };
   std::vector<std::shared_ptr<rclcpp::WallTimer<decltype(lambda)>>> timers;
   // creating 20 timers which will try to do 1 ms of work each
@@ -72,12 +72,12 @@ TEST(CLASSNAME(test_executor, RMW_IMPLEMENTATION), spin_some_max_duration) {
   }
   executor.add_node(node);
 
-  const auto max_duration = 10ms;
+  const auto max_duration = 1s;
   const auto start = std::chrono::steady_clock::now();
   executor.spin_some(max_duration);
   const auto end = std::chrono::steady_clock::now();
   ASSERT_LT(max_duration, end - start);
-  ASSERT_GT(max_duration + 5ms, end - start);
+  ASSERT_GT(max_duration + 100ms, end - start);
 }
 
 TEST(CLASSNAME(test_executor, RMW_IMPLEMENTATION), multithreaded_spin_call) {
