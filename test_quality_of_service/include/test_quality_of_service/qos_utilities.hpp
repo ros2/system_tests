85376e9b535095c834f0bb1f34601d1f4d749d0f
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-12-20 16:27:37 +0100

    skip tests
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- test_quality_of_service/include/test_quality_of_service/qos_utilities.hpp
+++ test_quality_of_service/include/test_quality_of_service/qos_utilities.hpp
@@ -83,8 +83,15 @@ protected:
 
 class QosRclcppTestFixture : public BaseQosRclcppTestFixture
 {
+public:
+  QosRclcppTestFixture()
+  {
+    rmw_implementation_str = std::string(rmw_get_implementation_identifier());
+  }
+
 protected:
   const std::string this_rmw_implementation{RCUTILS_STRINGIFY(RMW_IMPLEMENTATION)};
+  std::string rmw_implementation_str;
 };
 
 #endif  // TEST_QUALITY_OF_SERVICE__QOS_UTILITIES_HPP_
